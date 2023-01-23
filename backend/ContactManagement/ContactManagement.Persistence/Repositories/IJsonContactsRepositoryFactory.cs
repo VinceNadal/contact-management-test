e963a7656f2b7593296948f84fe7adb54926da90
Vincent Nadal <vnadal@fullscale.io>
2023-01-24 00:22:49 +0800

    Create Factory for asynchronous initialization of repository

---
+++ backend/ContactManagement/ContactManagement.Persistence/Repositories/IJsonContactsRepositoryFactory.cs
@@ -0,0 +1,16 @@
+﻿using ContactManagement.Application.Contracts.Persistence;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManagement.Persistence.Repositories
+{
+    public interface IJsonContactsRepositoryFactory
+    {
+        IContactRepository CreateRepository(ILogger<JsonContactsRepository> logger, string jsonFilePath);
+        Task InitializeRepositoryAsync(IContactRepository repository);
+    }
+}
---
+++ backend/ContactManagement/ContactManagement.Persistence/Repositories/JsonContactRepositoryFactory.cs
@@ -0,0 +1,26 @@
+﻿using ContactManagement.Application.Contracts.Persistence;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManagement.Persistence.Repositories
+{
+    public class JsonContactRepositoryFactory : IJsonContactsRepositoryFactory
+    {
+        public IContactRepository CreateRepository(ILogger<JsonContactsRepository> logger, string jsonFilePath)
+        {
+            return new JsonContactsRepository(logger, jsonFilePath);
+        }
+
+        public async Task InitializeRepositoryAsync(IContactRepository repository)
+        {
+            if (repository is JsonContactsRepository jsonContactRepository)
+            {
+                await jsonContactRepository.InitializeAsync();
+            }
+        }
+    }
+}
