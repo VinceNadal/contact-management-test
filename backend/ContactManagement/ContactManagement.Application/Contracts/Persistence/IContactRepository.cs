341ad004603ee1977797b3d1ee5e3232a9de6ee7
Vincent Nadal <vnadal@fullscale.io>
2023-01-24 00:21:34 +0800

    Create interface for contact repository

---
+++ backend/ContactManagement/ContactManagement.Application/Contracts/Persistence/IContactRepository.cs
@@ -0,0 +1,19 @@
+﻿using ContactManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManagement.Application.Contracts.Persistence
+{
+    public interface IContactRepository
+    {
+        List<Contact> GetAllContacts();
+        Contact? GetContactById(Guid contactId);
+        void AddContact(Contact contact);
+        void UpdateContact(Contact contact);
+        void DeleteContact(Guid contactId);
+        Task<bool> SaveChanges();
+    }
+}
