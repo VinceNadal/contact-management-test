23e40ada976ebdf8ba2627e9e7c8a725ce0c28b9
Vincent Nadal <vnadal@fullscale.io>
2023-01-24 00:24:55 +0800

    Create extension method to register services in the application layer

---
+++ backend/ContactManagement/ContactManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using ContactManagement.Application.Contracts.Services;
+using ContactManagement.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManagement.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IContactService, JsonFileContactsService>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            
+            return services;
+        }
+    }
+}
