3863113b73cc11eb9a356d4ce771e80f1b0585a2
Vincent Nadal <vnadal@fullscale.io>
2023-01-24 00:24:32 +0800

    Create mapping profile for Dtos to domain entities

---
+++ backend/ContactManagement/ContactManagement.Application/MappingProfiles/ContactsProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ContactManagement.Application.Dtos;
+using ContactManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManagement.Application.MappingProfiles
+{
+    public class ContactsProfile : Profile
+    {
+        public ContactsProfile()
+        {
+            CreateMap<Contact, ContactDto>().ReverseMap();
+            CreateMap<UpdateContactDto, Contact>();
+            CreateMap<CreateContactDto, Contact>();
+        }
+    }
+}
