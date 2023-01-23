dd01af5341eb9b596260220d1bb573da04e86ad4
Vincent Nadal <vnadal@fullscale.io>
2023-01-23 23:26:37 +0800

    Change id type to GUID

--- backend/ContactManagement/ContactManagement.Domain/Entities/Contact.cs
+++ backend/ContactManagement/ContactManagement.Domain/Entities/Contact.cs
@@ -8,7 +8,7 @@ namespace ContactManagement.Domain.Entities
 {
     public class Contact
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
