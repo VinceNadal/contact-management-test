edf503d533093bc4f5bcedfcc51da1cc2ab5ec60
Vincent Nadal <vnadal@fullscale.io>
2023-01-30 17:04:47 +0800

    Initial creation of frontend app

---
+++ frontend/src/Features/Contacts.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import { Contact } from "../Interfaces/Contact";
+import { getContacts } from "../Services/Api";
+
+// Create Contacts component that renders a list of contacts from the API using the getContacts from the Api.ts file
+export const Contacts = () => {
+  const [contacts, setContacts] = useState<Contact[]>([]);
+
+  // call  getContacts function from the Api.ts file with async await inside a useEffect
+  useEffect(() => {
+    const fetchContacts = async () => {
+      const contacts = await getContacts();
+      setContacts(contacts);
+    };
+    fetchContacts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Contacts</h1>
+      <ul>
+        {contacts.map((contact) => (
+          <div key={contact.id}>
+            <li>{contact.firstName}</li>
+            <li>{contact.lastName}</li>
+            <li>{contact.email}</li>
+            <li>{contact.billingAddress}</li>
+            <li>{contact.deliveryAddress}</li>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
